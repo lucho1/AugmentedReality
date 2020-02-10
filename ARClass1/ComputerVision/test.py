388ab7f0fd7099da6afe4702e63ad06b4ea975aa
lucho1 <luchosuaya@hotmail.com>
2020-02-10 10:20:46 +0100

    1st commit, VS Code & Python instalation
    Also, added Pylint into code and make a virtual python machine in directory

---
+++ ARClass1/ComputerVision/test.py
@@ -0,0 +1,7 @@
+print("patata")
+
+# We installed code as a zip for 64 bit in this folder. Then, with a couple
+# of CMD console commands, we have installed a virtual python machine in the
+# directory. Then, open the directory, here in Code (Open Folder) and click
+# down left in the "Python 3.7.4-bit" label and select the directory. 
+# Finally, we have installed Pylint.
