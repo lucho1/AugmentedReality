bffdd0a7fb149ecbad74a72d67773acf20a5b18a
lucho1 <luchosuaya@hotmail.com>
2020-03-27 12:37:12 +0100

    Added Result Image

--- ARClass1/ComputerVision/CVEX.py
+++ ARClass1/ComputerVision/CVEX.py
@@ -5,6 +5,10 @@ import numpy as np
 default_font = cv2.QT_FONT_NORMAL
 
 # ---------------------------------------------------------------
+#Inner Variables
+match = False
+
+# ---------------------------------------------------------------
 #Inputs
 #input_imgPath = input("Introduce Path for Input Image: ")           #ex_img/img1.png
 #target_imgPath = input("Introduce Path for Target Image: ")         #ex_img/t1-img1.png
@@ -24,10 +28,24 @@ print(msg3)
 # ---------------------------------------------------------------
 #Functions
 
+
+
 # ---------------------------------------------------------------
 #Load Images
 input_img = cv2.imread(input_imgPath, cv2.IMREAD_COLOR)
 target_img = cv2.imread(target_imgPath, cv2.IMREAD_COLOR)
+
+#Setup Stuff for Text Window
+result_img = np.zeros((40, 320, 4), np.uint8)   #Black Image
+
+result_msg = "TARGET NOT FOUND"                 #Text Mesage
+result_text_color = (0, 0, 255)                 #Text Color
+
+if match:                                       #Changing Text if match found
+    result_msg = "TARGET FOUND"
+    result_text_color = (0, 255, 0)
+
+cv2.putText(result_img, result_msg, (5, 30), default_font, 1, result_text_color, 1)     #Put text into image
 
 # ---------------------------------------------------------------
 #Display Images
@@ -38,6 +56,13 @@ cv2.imshow(winName, input_img)
 winName = 'Target Image'
 cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
 cv2.imshow(winName, target_img)
+
+winName = 'Matching Map Image'
+cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+
+winName = 'Result Mesage'
+cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+cv2.imshow(winName, result_img)
 
 # ---------------------------------------------------------------
 #Keep the Screen Showing
