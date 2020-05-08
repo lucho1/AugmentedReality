b744b38693dc8b18003bbac814ae30bf08e10f31
lucho1 <luchosuaya@hotmail.com>
2020-05-08 18:59:40 +0200

    Abstracted Scene & Cleaned Main

---
+++ ARClass1/ComputerGraphics/WebGL/Scene.js
@@ -0,0 +1,78 @@
+class Scene
+{
+    #m_MeshesToDraw = [];
+    #m_MeshesDrawSize = 0;
+
+    constructor()
+    {
+        this.getMeshesToDraw = function() { return this.#m_MeshesToDraw; }
+        this.getMeshesVecSize = function() { return this.#m_MeshesDrawSize; }
+    }
+
+    //Load the Scene
+    LoadGeometry = function(shader)
+    {
+        var tri1 = new Mesh();
+        tri1.LoadTriangle(shader);
+        tri1.SetPosition([0.0, 1.5, -10.0]);
+        tri1.SetMeshColor([0.0, 1.0, 0.5, 1.0]);
+
+        var sq1 = new Mesh();
+        sq1.LoadSquare(shader);
+        sq1.SetPosition([3.0, 0.0, -10.0]);
+        sq1.SetMeshColor([0.0, 0.5, 1.0, 1.0]);
+
+        var tri2 = new Mesh();
+        tri2.LoadTriangle(shader);
+        tri2.SetPosition([0.0, -1.5, -10.0]);
+        tri2.SetMeshColor([1.0, 0.0, 0.5, 1.0]);
+
+        var sq2 = new Mesh();
+        sq2.LoadSquare(shader);
+        sq2.SetPosition([-3.0, 0.0, -10.0]);
+        sq2.SetMeshColor([1.0, 1.0, 0.5, 1.0]);
+        sq2.SetRotation(45, [0.0, 0.0, 1.0]);
+
+        this.#m_MeshesToDraw = new Array(tri1, tri2, sq1, sq2);
+        this.#m_MeshesDrawSize = this.#m_MeshesToDraw.length;
+    }
+
+    //Draw the Scene
+    DrawScene = function(mainCamera, shader, renderer)
+    {
+        //Pre-rendering orders
+        gl.enable(gl.DEPTH_TEST);
+        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        
+        //Rendering Variables Setup
+        var pMatrix = mat4.create();
+        var mvMatrix = mat4.create();
+
+        mat4.identity(mvMatrix);
+        mat4.perspective(60, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+        //Camera Move
+        var cameraPos = vec3.create(mainCamera.getPosition());
+        mat4.translate(mvMatrix, vec3.negate(cameraPos));
+        mat4.rotate(mvMatrix, mainCamera.getXRotation(), [1.0, 0.0, 0.0]);
+        mat4.rotate(mvMatrix, mainCamera.getYRotation(), [0.0, 1.0, 0.0]);
+        
+        //Rendering preparation (Binding & Uniforms)
+        shader.BindShader();
+        shader.SetUniformMat4f("u_ProjMatrix", pMatrix);    
+        shader.SetUniformMat4f("u_ViewMatrix", mvMatrix);
+
+        //Rendering
+        var i = 0;
+        while(i < this.#m_MeshesDrawSize)
+        {        
+            renderer.DrawMesh(this.#m_MeshesToDraw[i], shader);
+            ++i;      
+        }
+
+        shader.UnbindShader();
+    }
+
+}
--- ARClass1/ComputerGraphics/WebGL/main.js
+++ ARClass1/ComputerGraphics/WebGL/main.js
@@ -1,7 +1,7 @@
 //Global variables
 var gl;
-var renderer, AppInput, MainCamera;
-var DrawMeshes, meshesSize, defShader;
+var Renderer, AppInput, MainCamera, MainScene;
+var defShader;
 
 //Delta Time
 var time = { dt: 1.0/0.0, lastTimeMS: 0}
@@ -17,51 +17,7 @@ function RADTODEG(rad) { return 180.0*rad/PI; }
 function mod(val1, val2) { return val1%val2; }
 function clamp(val1, min, max) { return val1 < min ? min : (val1 > max ? max : val1); }
 
-//Time Initialization
-function InitTime()
-{
-    var d = new Date();
-    time.lastTimeMS = d.getTime();
-}
-
-//Draw the Scene
-function DrawScene()
-{
-    //Pre-rendering orders
-    gl.enable(gl.DEPTH_TEST);
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    //Rendering Variables Setup
-    var pMatrix = mat4.create();
-    var mvMatrix = mat4.create();
-
-    mat4.identity(mvMatrix);
-    mat4.perspective(60, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
-
-    //Camera Move
-    var cameraPos = vec3.create(MainCamera.getPosition());
-    mat4.translate(mvMatrix, vec3.negate(cameraPos));
-    mat4.rotate(mvMatrix, MainCamera.getXRotation(), [1.0, 0.0, 0.0]);
-    mat4.rotate(mvMatrix, MainCamera.getYRotation(), [0.0, 1.0, 0.0]);
-    
-    //Rendering preparation (Binding & Uniforms)
-    defShader.BindShader();
-    defShader.SetUniformMat4f("u_ProjMatrix", pMatrix);    
-    defShader.SetUniformMat4f("u_ViewMatrix", mvMatrix);
-
-    //Rendering
-    var i = 0;
-    while(i < meshesSize)
-    {        
-        renderer.DrawMesh(DrawMeshes[i], defShader);
-        ++i;      
-    }
-
-    defShader.UnbindShader();
-}
-
+//Update Function
 function Update()
 {
     var d = new Date();
@@ -71,60 +27,37 @@ function Update()
 
     requestAnimationFrame(Update);
     MainCamera.MoveCamera(time.dt);
-    //ProcessInput();    
-    DrawScene();
+    MainScene.DrawScene(MainCamera, defShader, Renderer);
     AppInput.ResetInput();
 }
-
 
 // App's Main Loop
 var mainLoop = function()
 {
     //Time Start
-    InitTime();
+    var t = new Date();
+    time.lastTimeMS = t.getTime();
 
     //Create Renderer
-    renderer = new GLRenderer();
-    renderer.Init("screen_canvas", "webgl2"); //"experimental-webgl"
+    Renderer = new GLRenderer();
+    Renderer.Init("screen_canvas", "webgl2"); //"experimental-webgl"
 
     //Create Input & Main Camera
     AppInput = new Input();
     MainCamera = new Camera();
 
     //Setup Default Shader
-    renderer.CreateDefaultShader("DefaultVertexShader", "DefaultFragmentShader");    
-    defShader = renderer.getDefaultShader();
+    Renderer.CreateDefaultShader("DefaultVertexShader", "DefaultFragmentShader");    
+    defShader = Renderer.getDefaultShader();
 
-    //Setup meshes
-    tri1 = new Mesh();
-    tri1.LoadTriangle(defShader);
-    tri1.SetPosition([0.0, 1.5, -10.0]);
-    tri1.SetMeshColor([0.0, 1.0, 0.5, 1.0]);
+    //Setup Main Scene
+    MainScene = new Scene();
+    MainScene.LoadGeometry(defShader);
 
-    sq1 = new Mesh();
-    sq1.LoadSquare(defShader);
-    sq1.SetPosition([3.0, 0.0, -10.0]);
-    sq1.SetMeshColor([0.0, 0.5, 1.0, 1.0]);
+    //Create Textures for meshes
+    MainScene.getMeshesToDraw()[0].SetMeshTexture(new Texture("images/wall.jpg", "texture1"));
+    //MainScene.getMeshesToDraw()[2].SetMeshTexture(new Texture("images/awesomeface.png", "texture2"));
 
-    tri2 = new Mesh();
-    tri2.LoadTriangle(defShader);
-    tri2.SetPosition([0.0, -1.5, -10.0]);
-    tri2.SetMeshColor([1.0, 0.0, 0.5, 1.0]);
-
-    sq2 = new Mesh();
-    sq2.LoadSquare(defShader);
-    sq2.SetPosition([-3.0, 0.0, -10.0]);
-    sq2.SetMeshColor([1.0, 1.0, 0.5, 1.0]);
-    sq2.SetRotation(45, [0.0, 0.0, 1.0]);
-
-    //Create Textures
-    texture1 = new Texture("images/wall.jpg", "texture1");
-    tri1.SetMeshTexture(texture1);
-    //texture2 = new Texture("images/awesomeface.png", "texture2");
-    //sq2.SetMeshTexture(texture2);
-    
-    //Draw
-    DrawMeshes = new Array(tri1, tri2, sq1, sq2);
-    meshesSize = 4;
+    //Finally, Update
     Update();
 }
